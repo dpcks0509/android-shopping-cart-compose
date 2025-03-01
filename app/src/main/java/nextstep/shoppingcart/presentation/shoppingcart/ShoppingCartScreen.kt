21cae90fbb77e3503a9181e18fa05ecccff0c51f
dpcks0509 <dpcks0509@gmail.com>
2025-03-01 22:27:58 +0900

    refactor: ShoppingCart State -> StateFlow로 수정

--- app/src/main/java/nextstep/shoppingcart/presentation/shoppingcart/ShoppingCartScreen.kt
+++ app/src/main/java/nextstep/shoppingcart/presentation/shoppingcart/ShoppingCartScreen.kt
@@ -10,13 +10,14 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import nextstep.shoppingcart.domain.model.Product
@@ -32,13 +33,11 @@ fun ShoppingCartScreen(
     navController: NavController,
     viewModel: ShoppingCartViewModel = hiltViewModel(),
 ) {
-    LaunchedEffect(key1 = true) {
-        viewModel.loadShoppingCartProducts()
-    }
+    val state by viewModel.state.collectAsStateWithLifecycle()
 
     ShoppingCartContent(
         navController = navController,
-        state = viewModel.state,
+        state = state,
         onEvent = viewModel::onEvent
     )
 }
--- app/src/main/java/nextstep/shoppingcart/presentation/shoppingcart/ShoppingCartViewModel.kt
+++ app/src/main/java/nextstep/shoppingcart/presentation/shoppingcart/ShoppingCartViewModel.kt
@@ -1,22 +1,34 @@
 package nextstep.shoppingcart.presentation.shoppingcart
 
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
 import androidx.compose.runtime.toMutableStateList
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
 import nextstep.shoppingcart.domain.model.Product
 import nextstep.shoppingcart.domain.usecase.shoppingcart.ShoppingCartUseCase
 import javax.inject.Inject
 
 @HiltViewModel
 class ShoppingCartViewModel @Inject constructor(
-    private val shoppingCartUseCase: ShoppingCartUseCase
+    private val shoppingCartUseCase: ShoppingCartUseCase,
 ) : ViewModel() {
-    var state by mutableStateOf(ShoppingCartState())
-        private set
+    private val _state: MutableStateFlow<ShoppingCartState> =
+        MutableStateFlow(ShoppingCartState())
+    val state: StateFlow<ShoppingCartState> = _state
+        .onStart {
+            loadShoppingCartProducts()
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = ShoppingCartState(isLoading = true)
+        )
 
     fun onEvent(event: ShoppingCartEvent) {
         when (event) {
@@ -27,15 +39,15 @@ class ShoppingCartViewModel @Inject constructor(
         }
     }
 
-    fun loadShoppingCartProducts() {
+    private fun loadShoppingCartProducts() {
         shoppingCartUseCase.getShoppingCartProducts().fold(onSuccess = { shoppingCartProducts ->
-            state = state.copy(
+            _state.value = _state.value.copy(
                 shoppingCartProducts = shoppingCartProducts.toMutableStateList(),
                 isLoading = false,
                 error = null
             )
         }, onFailure = { error ->
-            state = state.copy(
+            _state.value = _state.value.copy(
                 shoppingCartProducts = mutableStateListOf(),
                 isLoading = false,
                 error = error.message
@@ -45,36 +57,36 @@ class ShoppingCartViewModel @Inject constructor(
 
     private fun addProduct(product: Product) {
         shoppingCartUseCase.addProduct(product)
-        val index = state.shoppingCartProducts.indexOfFirst { shoppingCartProduct ->
+        val index = _state.value.shoppingCartProducts.indexOfFirst { shoppingCartProduct ->
             shoppingCartProduct.product.id == product.id
         }
-        state.shoppingCartProducts[index] =
-            state.shoppingCartProducts[index].copy(quantity = state.shoppingCartProducts[index].quantity + 1)
+        _state.value.shoppingCartProducts[index] =
+            _state.value.shoppingCartProducts[index].copy(quantity = _state.value.shoppingCartProducts[index].quantity + 1)
     }
 
     private fun decreaseProductQuantity(product: Product) {
         shoppingCartUseCase.decreaseProductQuantity(product)
-        val index = state.shoppingCartProducts.indexOfFirst { shoppingCartProduct ->
+        val index = _state.value.shoppingCartProducts.indexOfFirst { shoppingCartProduct ->
             shoppingCartProduct.product.id == product.id
         }
-        if (state.shoppingCartProducts[index].quantity != 1) {
-            state.shoppingCartProducts[index] =
-                state.shoppingCartProducts[index].copy(quantity = state.shoppingCartProducts[index].quantity - 1)
+        if (_state.value.shoppingCartProducts[index].quantity != 1) {
+            _state.value.shoppingCartProducts[index] =
+                _state.value.shoppingCartProducts[index].copy(quantity = _state.value.shoppingCartProducts[index].quantity - 1)
         } else {
-            state.shoppingCartProducts.removeAt(index)
+            _state.value.shoppingCartProducts.removeAt(index)
         }
     }
 
     private fun removeProduct(product: Product) {
         shoppingCartUseCase.removeProduct(product)
-        val index = state.shoppingCartProducts.indexOfFirst { shoppingCartProduct ->
+        val index = _state.value.shoppingCartProducts.indexOfFirst { shoppingCartProduct ->
             shoppingCartProduct.product.id == product.id
         }
-        state.shoppingCartProducts.removeAt(index)
+        _state.value.shoppingCartProducts.removeAt(index)
     }
 
     private fun clearProducts() {
         shoppingCartUseCase.clearProducts()
-        state.shoppingCartProducts.clear()
+        _state.value.shoppingCartProducts.clear()
     }
 }
