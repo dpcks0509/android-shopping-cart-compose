bd5a230185f353ca1c387ae4f124d7385cc852a4
dpcks0509 <dpcks0509@gmail.com>
2025-03-03 02:31:10 +0900

    refactor: ShoppingCart update 및 flow 함수 사용

--- app/src/main/java/nextstep/shoppingcart/presentation/shoppingcart/ShoppingCartViewModel.kt
+++ app/src/main/java/nextstep/shoppingcart/presentation/shoppingcart/ShoppingCartViewModel.kt
@@ -10,92 +10,115 @@ import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
 import nextstep.shoppingcart.domain.model.Product
 import nextstep.shoppingcart.domain.usecase.shoppingcart.ShoppingCartUseCase
 import javax.inject.Inject
 
 @HiltViewModel
 class ShoppingCartViewModel
-    @Inject
-    constructor(
-        private val shoppingCartUseCase: ShoppingCartUseCase,
-    ) : ViewModel() {
-        private val _state: MutableStateFlow<ShoppingCartState> =
-            MutableStateFlow(ShoppingCartState())
-        val state: StateFlow<ShoppingCartState> =
-            _state
-                .onStart {
-                    loadShoppingCartProducts()
-                }
-                .stateIn(
-                    scope = viewModelScope,
-                    started = SharingStarted.WhileSubscribed(5000),
-                    initialValue = ShoppingCartState(isLoading = true),
-                )
-
-        fun onEvent(event: ShoppingCartEvent) {
-            when (event) {
-                is ShoppingCartEvent.AddProduct -> addProduct(event.product)
-                is ShoppingCartEvent.DecreaseProductQuantity -> decreaseProductQuantity(event.product)
-                is ShoppingCartEvent.RemoveProduct -> removeProduct(event.product)
-                is ShoppingCartEvent.ClearProducts -> clearProducts()
+@Inject
+constructor(
+    private val shoppingCartUseCase: ShoppingCartUseCase,
+) : ViewModel() {
+    private val _state: MutableStateFlow<ShoppingCartState> =
+        MutableStateFlow(ShoppingCartState())
+    val state: StateFlow<ShoppingCartState> =
+        _state
+            .onStart {
+                loadShoppingCartProducts()
             }
-        }
+            .stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(5000),
+                initialValue = ShoppingCartState(isLoading = true),
+            )
 
-        private fun loadShoppingCartProducts() {
-            shoppingCartUseCase.getShoppingCartProducts().fold(onSuccess = { shoppingCartProducts ->
-                _state.value =
-                    _state.value.copy(
-                        shoppingCartProducts = shoppingCartProducts.toMutableStateList(),
-                        isLoading = false,
-                        error = null,
-                    )
-            }, onFailure = { error ->
-                _state.value =
-                    _state.value.copy(
-                        shoppingCartProducts = mutableStateListOf(),
-                        isLoading = false,
-                        error = error.message,
-                    )
-            })
-        }
-
-        private fun addProduct(product: Product) {
-            shoppingCartUseCase.addProduct(product)
-            val index =
-                _state.value.shoppingCartProducts.indexOfFirst { shoppingCartProduct ->
-                    shoppingCartProduct.product.id == product.id
-                }
-            _state.value.shoppingCartProducts[index] =
-                _state.value.shoppingCartProducts[index].copy(quantity = _state.value.shoppingCartProducts[index].quantity + 1)
-        }
-
-        private fun decreaseProductQuantity(product: Product) {
-            shoppingCartUseCase.decreaseProductQuantity(product)
-            val index =
-                _state.value.shoppingCartProducts.indexOfFirst { shoppingCartProduct ->
-                    shoppingCartProduct.product.id == product.id
-                }
-            if (_state.value.shoppingCartProducts[index].quantity != 1) {
-                _state.value.shoppingCartProducts[index] =
-                    _state.value.shoppingCartProducts[index].copy(quantity = _state.value.shoppingCartProducts[index].quantity - 1)
-            } else {
-                shoppingCartUseCase.removeProduct(product)
-                _state.value.shoppingCartProducts.removeAt(index)
-            }
-        }
-
-        private fun removeProduct(product: Product) {
-            shoppingCartUseCase.removeProduct(product)
-            val index =
-                _state.value.shoppingCartProducts.indexOfFirst { shoppingCartProduct ->
-                    shoppingCartProduct.product.id == product.id
-                }
-            _state.value.shoppingCartProducts.removeAt(index)
-        }
-
-        private fun clearProducts() {
-            shoppingCartUseCase.clearProducts()
-            _state.value.shoppingCartProducts.clear()
+    fun onEvent(event: ShoppingCartEvent) {
+        when (event) {
+            is ShoppingCartEvent.AddProduct -> addProduct(event.product)
+            is ShoppingCartEvent.DecreaseProductQuantity -> decreaseProductQuantity(event.product)
+            is ShoppingCartEvent.RemoveProduct -> removeProduct(event.product)
+            is ShoppingCartEvent.ClearProducts -> clearProducts()
         }
     }
+
+    private fun loadShoppingCartProducts() {
+        shoppingCartUseCase.getShoppingCartProducts().fold(onSuccess = { shoppingCartProducts ->
+            _state.update { currentState ->
+                currentState.copy(
+                    shoppingCartProducts = shoppingCartProducts.toMutableStateList(),
+                    isLoading = false,
+                    error = null,
+                )
+            }
+        }, onFailure = { error ->
+            _state.update { currentState ->
+                currentState.copy(
+                    shoppingCartProducts = mutableStateListOf(),
+                    isLoading = false,
+                    error = error.message,
+                )
+            }
+        })
+    }
+
+    private fun addProduct(product: Product) {
+        shoppingCartUseCase.addProduct(product)
+
+        _state.update { currentState ->
+            val updatedProducts = currentState.shoppingCartProducts.map { shoppingCartProduct ->
+                if (shoppingCartProduct.product.id == product.id) {
+                    shoppingCartProduct.copy(quantity = shoppingCartProduct.quantity + 1)
+                } else {
+                    shoppingCartProduct
+                }
+            }
+
+            currentState.copy(shoppingCartProducts = updatedProducts.toMutableStateList())
+        }
+    }
+
+    private fun decreaseProductQuantity(product: Product) {
+        shoppingCartUseCase.decreaseProductQuantity(product)
+
+        _state.update { currentState ->
+            val updatedProducts =
+                currentState.shoppingCartProducts.mapNotNull { shoppingCartProduct ->
+                    if (shoppingCartProduct.product.id == product.id) {
+                        if (shoppingCartProduct.quantity > 1) {
+                            shoppingCartProduct.copy(quantity = shoppingCartProduct.quantity - 1)
+                        } else {
+                            shoppingCartUseCase.removeProduct(product)
+                            null
+                        }
+                    } else {
+                        shoppingCartProduct
+                    }
+                }
+
+            currentState.copy(shoppingCartProducts = updatedProducts.toMutableStateList())
+        }
+    }
+
+    private fun removeProduct(product: Product) {
+        shoppingCartUseCase.removeProduct(product)
+
+        _state.update { currentState ->
+            val updatedProducts =
+                currentState.shoppingCartProducts.filterNot { shoppingCartProduct ->
+                    shoppingCartProduct.product.id == product.id
+                }
+
+            currentState.copy(shoppingCartProducts = updatedProducts.toMutableStateList())
+        }
+    }
+
+    private fun clearProducts() {
+        shoppingCartUseCase.clearProducts()
+
+        _state.update { currentState ->
+            currentState.copy(shoppingCartProducts = mutableStateListOf())
+        }
+    }
+}
