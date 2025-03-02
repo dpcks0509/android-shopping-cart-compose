bc3a3049977f673fd30a44a46f33fd0f20aef340
dpcks0509 <dpcks0509@gmail.com>
2025-03-03 02:49:51 +0900

    refactor: ProductDetail update 방법 변경

--- app/src/main/java/nextstep/shoppingcart/presentation/productdetail/ProductDetailViewModel.kt
+++ app/src/main/java/nextstep/shoppingcart/presentation/productdetail/ProductDetailViewModel.kt
@@ -10,6 +10,7 @@ import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import nextstep.shoppingcart.domain.model.Product
 import nextstep.shoppingcart.domain.usecase.product.ProductUseCase
@@ -19,60 +20,65 @@ import javax.inject.Inject
 
 @HiltViewModel
 class ProductDetailViewModel
-    @Inject
-    constructor(
-        private val productUseCase: ProductUseCase,
-        private val shoppingCartUseCase: ShoppingCartUseCase,
-        savedStateHandle: SavedStateHandle,
-    ) : ViewModel() {
-        val productId = savedStateHandle.toRoute<Screen.ProductDetailScreen>().productId
+@Inject
+constructor(
+    private val productUseCase: ProductUseCase,
+    private val shoppingCartUseCase: ShoppingCartUseCase,
+    savedStateHandle: SavedStateHandle,
+) : ViewModel() {
+    val productId = savedStateHandle.toRoute<Screen.ProductDetailScreen>().productId
 
-        private val _state: MutableStateFlow<ProductDetailState> =
-            MutableStateFlow(ProductDetailState())
-        val state: StateFlow<ProductDetailState> =
-            _state
-                .onStart {
-                    loadProduct(productId)
-                }
-                .stateIn(
-                    scope = viewModelScope,
-                    started = SharingStarted.WhileSubscribed(5000),
-                    initialValue = ProductDetailState(isLoading = true),
-                )
-
-        fun onEvent(event: ProductDetailEvent) {
-            when (event) {
-                is ProductDetailEvent.AddProduct -> {
-                    addProduct(product = event.product)
-                }
+    private val _state: MutableStateFlow<ProductDetailState> =
+        MutableStateFlow(ProductDetailState())
+    val state: StateFlow<ProductDetailState> =
+        _state
+            .onStart {
+                loadProduct(productId)
             }
-        }
+            .stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(5000),
+                initialValue = ProductDetailState(isLoading = true),
+            )
 
-        private fun loadProduct(productId: Long) {
-            viewModelScope.launch {
-                productUseCase.getProduct(productId = productId).fold(
-                    onSuccess = { product ->
-                        _state.value =
-                            _state.value.copy(
-                                product = product,
-                                isLoading = false,
-                                error = null,
-                            )
-                    },
-                    onFailure = {
-                        _state.value =
-                            _state.value.copy(
-                                product = null,
-                                isLoading = false,
-                                error = null,
-                            )
-                    },
-                )
+    fun onEvent(event: ProductDetailEvent) {
+        when (event) {
+            is ProductDetailEvent.AddProduct -> {
+                addProduct(product = event.product)
             }
-        }
-
-        private fun addProduct(product: Product) {
-            shoppingCartUseCase.addProduct(product = product)
-            _state.value = _state.value.copy(product = product)
         }
     }
+
+    private fun loadProduct(productId: Long) {
+        viewModelScope.launch {
+            productUseCase.getProduct(productId = productId).fold(
+                onSuccess = { product ->
+                    _state.update { currentState ->
+                        currentState.copy(
+                            product = product,
+                            isLoading = false,
+                            error = null,
+                        )
+                    }
+                },
+                onFailure = { error ->
+                    _state.update { currentState ->
+                        currentState.copy(
+                            product = null,
+                            isLoading = false,
+                            error = error.message,
+                        )
+                    }
+                },
+            )
+        }
+    }
+
+    private fun addProduct(product: Product) {
+        shoppingCartUseCase.addProduct(product = product)
+
+        _state.update { currentState ->
+            currentState.copy(product = product)
+        }
+    }
+}
